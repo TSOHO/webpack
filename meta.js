142cd52ec28b8ef009c784e3bb3e0fe20fae7cf7
h.yingzi <h.yingzi@gmail.com>
2018-08-10 20:14:42 +0800

    package fix

--- meta.js
+++ meta.js
@@ -1,12 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 
-const {
-  sortDependencies,
-  installDependencies,
-  runLintFix,
-  printMessage,
-} = require('./utils')
+const {sortDependencies, installDependencies, runLintFix, printMessage} = require('./utils')
 const pkg = require('./package.json')
 
 const templateVersion = pkg.version
@@ -20,7 +15,6 @@ module.exports = {
   },
   helpers: {
     if_or(v1, v2, options) {
-
       if (v1 || v2) {
         return options.fn(this)
       }
@@ -29,7 +23,7 @@ module.exports = {
     },
     template_version() {
       return templateVersion
-    },
+    }
   },
 
   prompts: {
@@ -37,19 +31,19 @@ module.exports = {
       when: 'isNotTest',
       type: 'string',
       required: true,
-      message: 'Project name',
+      message: 'Project name'
     },
     description: {
       when: 'isNotTest',
       type: 'string',
       required: false,
       message: 'Project description',
-      default: 'A Vue.js project',
+      default: 'A Vue.js project'
     },
     author: {
       when: 'isNotTest',
       type: 'string',
-      message: 'Author',
+      message: 'Author'
     },
     build: {
       when: 'isNotTest',
@@ -59,44 +53,101 @@ module.exports = {
         {
           name: 'Runtime + Compiler: recommended for most users',
           value: 'standalone',
-          short: 'standalone',
+          short: 'standalone'
         },
         {
-          name:
-            'Runtime-only: about 6KB lighter min+gzip, but templates (or any Vue-specific HTML) are ONLY allowed in .vue files - render functions are required elsewhere',
+          name: 'Runtime-only: about 6KB lighter min+gzip, but templates (or any Vue-specific HTML) are ONLY allowed in .vue files - render functions are required elsewhere',
           value: 'runtime',
-          short: 'runtime',
-        },
-      ],
+          short: 'runtime'
+        }
+      ]
     },
     router: {
       when: 'isNotTest',
       type: 'confirm',
-      message: 'Install vue-router?',
+      message: 'Install vue-router?'
+    },
+    lint: {
+      when: 'isNotTest',
+      type: 'confirm',
+      message: 'Use ESLint to lint your code?'
+    },
+    lintConfig: {
+      when: 'isNotTest && lint',
+      type: 'list',
+      message: 'Pick an ESLint preset',
+      choices: [
+        {
+          name: 'Standard (https://github.com/standard/standard)',
+          value: 'standard',
+          short: 'Standard'
+        },
+        {
+          name: 'Airbnb (https://github.com/airbnb/javascript)',
+          value: 'airbnb',
+          short: 'Airbnb'
+        },
+        {
+          name: 'none (configure it yourself)',
+          value: 'none',
+          short: 'none'
+        }
+      ]
+    },
+    unit: {
+      when: 'isNotTest',
+      type: 'confirm',
+      message: 'Set up unit tests'
+    },
+    runner: {
+      when: 'isNotTest && unit',
+      type: 'list',
+      message: 'Pick a test runner',
+      choices: [
+        {
+          name: 'Jest',
+          value: 'jest',
+          short: 'jest'
+        },
+        {
+          name: 'Karma and Mocha',
+          value: 'karma',
+          short: 'karma'
+        },
+        {
+          name: 'none (configure it yourself)',
+          value: 'noTest',
+          short: 'noTest'
+        }
+      ]
+    },
+    e2e: {
+      when: 'isNotTest',
+      type: 'confirm',
+      message: 'Setup e2e tests with Nightwatch?'
     },
     autoInstall: {
       when: 'isNotTest',
       type: 'list',
-      message:
-        'Should we run `npm install` for you after the project has been created? (recommended)',
+      message: 'Should we run `npm install` for you after the project has been created? (recommended)',
       choices: [
         {
           name: 'Yes, use NPM',
           value: 'npm',
-          short: 'npm',
+          short: 'npm'
         },
         {
           name: 'Yes, use Yarn',
           value: 'yarn',
-          short: 'yarn',
+          short: 'yarn'
         },
         {
           name: 'No, I will handle that myself',
           value: false,
-          short: 'no',
-        },
-      ],
-    },
+          short: 'no'
+        }
+      ]
+    }
   },
   filters: {
     '.eslintrc.js': 'lint',
@@ -110,9 +161,9 @@ module.exports = {
     'test/unit/specs/index.js': "unit && runner === 'karma'",
     'test/unit/setup.js': "unit && runner === 'jest'",
     'test/e2e/**/*': 'e2e',
-    'src/router/**/*': 'router',
+    'src/router/**/*': 'router'
   },
-  complete: function(data, { chalk }) {
+  complete: function (data, { chalk }) {
     const green = chalk.green
 
     sortDependencies(data, green)
@@ -133,5 +184,5 @@ module.exports = {
     } else {
       printMessage(data, chalk)
     }
-  },
+  }
 }
